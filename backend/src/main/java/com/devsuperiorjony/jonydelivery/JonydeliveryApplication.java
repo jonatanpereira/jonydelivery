6b7ff9fd132390e392467c7c66abb89c92b74b81
jonatan <jonatanmarcelyn@gmail.com>
2021-01-10 15:40:26 -0300

    Project created

---
+++ backend/src/main/java/com/devsuperiorjony/jonydelivery/JonydeliveryApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperiorjony.jonydelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JonydeliveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JonydeliveryApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devsuperiorjony/jonydelivery/JonydeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperiorjony.jonydelivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JonydeliveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
