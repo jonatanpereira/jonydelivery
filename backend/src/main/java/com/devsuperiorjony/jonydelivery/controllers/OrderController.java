00c04ad7c7922687c974f5bbce70ae2332570cb3
jonatan <jonatanmarcelyn@gmail.com>
2021-03-30 13:05:43 -0300

    POST /orders

--- backend/src/main/java/com/devsuperiorjony/jonydelivery/controllers/OrderController.java
+++ backend/src/main/java/com/devsuperiorjony/jonydelivery/controllers/OrderController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.devsuperiorjony.jonydelivery.dto.OrderDTO;
 import com.devsuperiorjony.jonydelivery.services.OrderService;
@@ -30,8 +31,8 @@ public class OrderController {
 	@PostMapping
 	public ResponseEntity<OrderDTO> insert(@RequestBody OrderDTO dto){
 		dto = service.insert(dto);
-		URI uri = ServeletUriComponent
-		return ResponseEntity.ok().body(dto);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(dto.getId()).toUri();
+		return ResponseEntity.created(uri).body(dto);
 	}
 
 }
