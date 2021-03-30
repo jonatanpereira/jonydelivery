a95a10f31601245e13b397d29e22e159c9333993
jonatan <jonatanmarcelyn@gmail.com>
2021-03-30 12:09:10 -0300

    Return to work

--- backend/src/main/java/com/devsuperiorjony/jonydelivery/controllers/OrderController.java
+++ backend/src/main/java/com/devsuperiorjony/jonydelivery/controllers/OrderController.java
@@ -1,10 +1,13 @@
 package com.devsuperiorjony.jonydelivery.controllers;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,6 +25,13 @@ public class OrderController {
 	public ResponseEntity<List<OrderDTO>> fundAll() {
 		List<OrderDTO> list = service.findAll();
 		return ResponseEntity.ok().body(list);
+	}
+	
+	@PostMapping
+	public ResponseEntity<OrderDTO> insert(@RequestBody OrderDTO dto){
+		dto = service.insert(dto);
+		URI uri = ServeletUriComponent
+		return ResponseEntity.ok().body(dto);
 	}
 
 }
--- backend/src/main/java/com/devsuperiorjony/jonydelivery/services/OrderService.java
+++ backend/src/main/java/com/devsuperiorjony/jonydelivery/services/OrderService.java
@@ -1,5 +1,6 @@
 package com.devsuperiorjony.jonydelivery.services;
 
+import java.time.Instant;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -8,8 +9,12 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperiorjony.jonydelivery.dto.OrderDTO;
+import com.devsuperiorjony.jonydelivery.dto.ProductDTO;
 import com.devsuperiorjony.jonydelivery.entities.Order;
+import com.devsuperiorjony.jonydelivery.entities.OrderStatus;
+import com.devsuperiorjony.jonydelivery.entities.Product;
 import com.devsuperiorjony.jonydelivery.repositories.OrderRepository;
+import com.devsuperiorjony.jonydelivery.repositories.ProductRepository;
 
 @Service
 public class OrderService {
@@ -17,9 +22,24 @@ public class OrderService {
 	@Autowired
 	private OrderRepository repository;
 	
+	@Autowired
+	private ProductRepository productRepository;
+	
 	@Transactional(readOnly = true)
 	public List<OrderDTO> findAll() {
 		List<Order> list = repository.findOrdersWhitProducts();
 		return list.stream().map(x -> new OrderDTO(x)).collect(Collectors.toList());
 	}
+	
+	@Transactional
+	public OrderDTO insert(OrderDTO dto) {
+		Order order = new Order(null, dto.getAddress(), dto.getLatitude(), dto.getLongitude(), Instant.now(), OrderStatus.PENDING);
+		for(ProductDTO p: dto.getProducts()) {
+			Product product = productRepository.getOne(p.getId());
+			order.getProducts().add(product);
+		}
+		order = repository.save(order);
+		return new OrderDTO(order);
+	}
+	
 }
